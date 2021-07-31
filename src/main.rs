6d9c81e1b6b15c1f511a23fdae15438faf2a6eb7
Jonah <jonahshafran@gmail.com>
2021-07-31 15:43:02 -0500

    Runtime: Add Flag to Run Expression Outside of REPL
    This makes it easier for us to test the functionality of the interpreter
    as we can feed it test cases and examine any results.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 use crate::runtime::print_program;
 use crate::runtime::vm::VM;
-use crate::Flags::{ExecutionOutput, LexerOutput, ParserOutput, PrintProgram};
+use crate::Flags::{ExecutionOutput, LexerOutput, ParserOutput, PrintProgram, Test};
 use std::{env, io};
 
 mod lexer;
@@ -15,59 +15,88 @@ mod runtime;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let flags = parse_args(args);
+    let flags = parse_args(&args);
 
-    loop {
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_n) => {
-                if input.eq("h\r\n") || input.eq("h\n") {
-                    break;
-                }
-                let mut tokenizer = lexer::Tokenizer::new(input);
-                let tokens = tokenizer.tokenize();
-
-                if flags.contains(&LexerOutput) {
-                    for token in &tokens {
-                        println!("{}", token);
-                    }
-                }
-
-                let parser = parser::Parser::new(tokens);
-                let parse_tree = parser.parse();
-                match parse_tree {
-                    None => println!("Parse unsuccessful"),
-                    Some(root) => {
-                        if flags.contains(&ParserOutput) {
-                            parser::print_parse_tree(&root);
+    if flags.len() > 0 {
+        if let Test(index) = flags[0] {
+            execute_line(args[index].clone(), &flags)
+        } else {
+            loop {
+                let mut input = String::new();
+                match io::stdin().read_line(&mut input) {
+                    Ok(_) => {
+                        if input.eq("h\r\n") || input.eq("h\n") {
+                            break;
                         }
-                        let mut program: Vec<u8> = Vec::new();
-                        root.to_bytes(&mut program);
-                        if flags.contains(&PrintProgram) {
-                            println!("{:?}", program);
-                            print_program(&program);
-                        }
-
-                        let mut vm = VM::new(program, flags.contains(&ExecutionOutput));
-                        vm.execute();
+                        execute_line(input, &flags)
                     }
+                    Err(err) => println!("{}", err),
                 }
             }
-            Err(err) => println!("{}", err),
+        }
+    } else {
+        loop {
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    if input.eq("h\r\n") || input.eq("h\n") {
+                        break;
+                    }
+                    execute_line(input, &flags)
+                }
+                Err(err) => println!("{}", err),
+            }
         }
     }
 }
 
-fn parse_args(args: Vec<String>) -> Vec<Flags> {
+fn execute_line(input: String, flags: &Vec<Flags>) {
+    let mut tokenizer = lexer::Tokenizer::new(input);
+    let tokens = tokenizer.tokenize();
+
+    if flags.contains(&LexerOutput) {
+        for token in &tokens {
+            println!("{}", token);
+        }
+    }
+
+    let parser = parser::Parser::new(tokens);
+    let parse_tree = parser.parse();
+    match parse_tree {
+        None => println!("Parse unsuccessful"),
+        Some(root) => {
+            if flags.contains(&ParserOutput) {
+                parser::print_parse_tree(&root);
+            }
+            let mut program: Vec<u8> = Vec::new();
+            root.to_bytes(&mut program);
+            if flags.contains(&PrintProgram) {
+                println!("{:?}", program);
+                print_program(&program);
+            }
+
+            let mut vm = VM::new(program, flags.contains(&ExecutionOutput));
+            vm.execute();
+        }
+    }
+}
+
+fn parse_args<'a>(args: &Vec<String>) -> Vec<Flags> {
     let mut flags: Vec<Flags> = Vec::new();
-    for arg in &args[1..] {
-        match &arg[0..] {
+    let mut i = 1;
+    while i < args[1..].len() {
+        match &args[i][0..] {
             "--lexer" | "-l" => flags.push(LexerOutput),
             "--parser" | "-p" => flags.push(ParserOutput),
             "--program" | "-P" => flags.push(PrintProgram),
             "--execution" | "-e" => flags.push(ExecutionOutput),
-            _ => panic!("Unrecognized argument {}", arg),
+            "--run" | "-r" => {
+                flags.push(Test(i + 1));
+                i += 1
+            }
+            _ => panic!("Unrecognized argument {}", args[i]),
         }
+        i += 1
     }
 
     return flags;
@@ -79,4 +108,5 @@ enum Flags {
     ParserOutput,
     PrintProgram,
     ExecutionOutput,
+    Test(usize),
 }
